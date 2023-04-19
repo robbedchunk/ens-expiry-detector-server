2bfa326419c1736d30d948c3979a5455a304c47e
Pedro Stanzani <pedrohstanzani@gmail.com>
2023-04-19 06:58:34 -0300

    Allow the lookup of domains with over 3 characters

--- app.js
+++ app.js
@@ -1,38 +1,33 @@
-const express = require('express');
-const cache = require('memory-cache');
+const express = require("express");
+const cache = require("memory-cache");
 const app = express();
 const port = 3000;
-const { checkAllDomains, checkDomain } = require('./ens-utils');
-
+const { checkAllDomains, checkDomain } = require("./ens-utils");
 
 // Your checkAllDomains and checkDomain functions here
 
-app.get('/domain/:name', async (req, res) => {
+app.get("/domain/:name", async (req, res) => {
   const domain = req.params.name;
 
-  if (domain.length === 3) {
-    const cachedData = cache.get(domain);
+  const cachedData = cache.get(domain);
 
-    if (cachedData) {
-      res.json(cachedData);
-    } else {
-      const data = await checkDomain(domain);
-      cache.put(domain, data, 10 * 60 * 1000); // Cache for 10 minutes
-      res.json(data);
-    }
+  if (cachedData) {
+    res.json(cachedData);
   } else {
-    res.status(400).json({ error: 'Invalid domain length. Domain must be 3 characters long.' });
+    const data = await checkDomain(domain);
+    cache.put(domain, data, 10 * 60 * 1000); // Cache for 10 minutes
+    res.json(data);
   }
 });
 
-app.get('/all-domains', async (req, res) => {
-  const cachedData = cache.get('allDomains');
+app.get("/all-domains", async (req, res) => {
+  const cachedData = cache.get("allDomains");
 
   if (cachedData) {
     res.json(cachedData);
   } else {
     const data = await checkAllDomains();
-    cache.put('allDomains', data, 10 * 60 * 1000); // Cache for 10 minutes
+    cache.put("allDomains", data, 10 * 60 * 1000); // Cache for 10 minutes
     res.json(data);
   }
 });
