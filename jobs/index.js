5fb858181b23d5e434e38b7c9b1dc7c6b6e7dfec
Pedro Stanzani <pedrohstanzani@gmail.com>
2023-04-19 10:44:25 -0300

    Create updateDatabaseRecords() method

--- jobs/index.js
+++ jobs/index.js
@@ -1,5 +1,8 @@
+const { checkDomain } = require("./../services/domains");
+const Record = require("./../models/Record");
+
 const getThreeCharacterDomains = () => {
-  const chars = '0123456789abcdefghijklmnopqrstuvwxyz';
+  const chars = "0123456789abcdefghijklmnopqrstuvwxyz";
   const domains = [];
 
   for (let i = 0; i < chars.length; i++) {
@@ -11,13 +14,32 @@ const getThreeCharacterDomains = () => {
   }
 
   return domains;
-}
+};
 
-// const updateDatabaseRecords = () => {
-//   const domains = getThreeCharacterDomains();
+const updateDatabaseRecords = async () => {
+  const domains = getThreeCharacterDomains();
 
-//   for (let i = 0; i < domains.length; i++) {
-//     const domain = domains[i];
+  for (let i = 0; i < domains.length; i++) {
+    const domain = domains[i];
+    const availability = await checkDomain(domain);
+    
+    const record = new Record({
+      domain: domain,
+      available: availability.available,
+      owner: availability.owner,
+      expirationDate: availability.expirationDate,
+      lastUpdated: Date.now(),
+    });
 
-//   }
-// }
+    record
+      .save()
+      .then((savedRecord) => {
+        // console.log(savedRecord);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+};
+
+module.exports = { updateDatabaseRecords }
