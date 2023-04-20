ce112fe552c30d04a95b06f69c64cb538a3fcec7
MrBrilew01 <mrbrilew@gmail.com>
2023-04-20 15:07:49 -0300

    feature: adding csv

--- controllers/domains.js
+++ controllers/domains.js
@@ -1,6 +1,19 @@
+const { exportAllDomains } = require('../services/csv');
 const { checkDomain } = require('./../services/domains');
 
 const domainsRouter = require("express").Router();
+
+domainsRouter.get("/csv", async (req, res) => {
+  try {
+    const csv = await exportAllDomains()
+    res.setHeader('Content-Type', 'text/csv');
+    res.setHeader('Content-Disposition', 'attachment; filename=ens-domains.csv');
+    res.send(csv);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error exporting data as CSV');
+  }
+});
 
 domainsRouter.get("/:name", async (req, res) => {
   const name = req.params.name;
---
+++ services/csv.js
@@ -0,0 +1,20 @@
+const Record = require("../models/Record")
+const Papa = require('papaparse')
+
+const exportAllDomains = async () => {
+	const data = await Record.find({}, {_id: 0, _v: 0}).lean().exec()
+
+
+	const csv = Papa.unparse(data, {
+		header: true,
+		quotes: true,
+		delimiter: ',',
+		newline: '\n'
+		});
+
+	return csv
+}
+
+module.exports = {
+	exportAllDomains
+}
