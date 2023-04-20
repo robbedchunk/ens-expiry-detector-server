987fff1122b148b80d8c97f87b1fee3daca0b6bd
Pedro Stanzani <pedrohstanzani@gmail.com>
2023-04-20 14:47:28 -0300

    Setup cron job

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const app = require("./app");
 const config = require('./utils/config')
+const cron = require('node-cron');
+
+const { updateDatabaseRecords } = require("./jobs/index");
+
+cron.schedule('0 18 * * *', () => {
+  updateDatabaseRecords();
+});
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`);
