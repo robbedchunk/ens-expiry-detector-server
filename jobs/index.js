73fd3e026633491a1537e2f1dfbbbeed9ffdb0ba
Pedro Stanzani <pedrohstanzani@gmail.com>
2023-04-20 12:17:17 -0300

    Modify updateDatabaseRecords() to reflect business requirement

--- jobs/index.js
+++ jobs/index.js
@@ -21,25 +21,58 @@ const updateDatabaseRecords = async () => {
 
   for (let i = 0; i < domains.length; i++) {
     const domain = domains[i];
-    const availability = await checkDomain(domain);
-    
-    const record = new Record({
-      domain: domain,
-      available: availability.available,
-      owner: availability.owner,
-      expirationDate: availability.expirationDate,
-      lastUpdated: Date.now(),
-    });
 
-    record
-      .save()
-      .then((savedRecord) => {
-        // console.log(savedRecord);
-      })
-      .catch((error) => {
-        console.log(error);
+    // Is domain already in the database?
+    const record = await Record.findOne({ domain: domain });
+
+    if (record) {
+      console.log(`[${domain}.eth] Domain was found in the database.`);
+
+      const twoDays = 48 * 60 * 60 * 1000;
+      const checkCondition =
+        record.available || record.expirationDate <= Date.now() + twoDays;
+
+      if (checkCondition) {
+        console.log(`[${domain}.eth] Criteria (availability or recent expiry) was matched. Searching availability.`);
+
+        const availability = await checkDomain(domain);
+        record.available = availability.available;
+        record.owner = availability.owner;
+        record.expirationDate = availability.expirationDate;
+        record.lastUpdated = Date.now();
+        record
+          .save()
+          .then((savedRecord) => {
+            console.log(`[${domain}.eth] Updated record and saved information to the database.`);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    } else {
+      console.log(`[${domain}.eth] Domain was not found in the database.`);
+      const availability = await checkDomain(domain);
+
+      const newRecord = new Record({
+        domain: domain,
+        available: availability.available,
+        owner: availability.owner,
+        expirationDate: availability.expirationDate,
+        lastUpdated: Date.now(),
       });
+
+      newRecord
+        .save()
+        .then((savedRecord) => {
+          console.log(`[${domain}.eth] Created record.`);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+
+    console.log("\n");
   }
 };
 
-module.exports = { updateDatabaseRecords }
+module.exports = { updateDatabaseRecords };
